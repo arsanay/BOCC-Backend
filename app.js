314b9f8d66ea859d6f7cbf842ad704d1368011bd
arsanay <arsanay@gmail.com>
2018-12-04 13:34:52 +0700

    Checkpoint1

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+const config = {useNewUrlParser: true}
+require('dotenv').config()
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('login');
+});
+
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8081);
+console.log('8081 is the magic port');
+
+const DB_NAME = process.env.DB_NAME;
+console.log(DB_NAME);
+mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`,
+    config,
+);
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method ==   'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUTS, POST, PATCH, DELETE');
+    }   
+    next();
+});
+
+//Routes which should handle any request
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes)
+app.use('/user',userRoutes)
+app.use(morgan('dev'));
+
+
+app.use((req, res, next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500 );
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+module.exports =  app  ;
